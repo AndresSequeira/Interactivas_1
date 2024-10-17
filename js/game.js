fe21f79ccf59d4997766f5bc39d5d3e78092c202
AndresSequeira <andresxdsequeira@gmail.com>
2024-10-17 10:09:19 -0600

    animations added and background painted

--- js/game.js
+++ js/game.js
@@ -1,35 +1,63 @@
 // Game.js
-import Player from './Player.js';
-import Enemy from './Enemy.js';
+import Player from "./Player.js";
+import Enemy from "./Enemy.js";
 
 export default class Game extends Phaser.Scene {
-    constructor() {
-        super({ key: 'Game' });
-    }
+  constructor() {
+    super({ key: "Game" });
+  }
 
-    preload() {
-        this.load.image('player', './img/assets/walk.png');
-        this.load.image('enemy', './img/assets/fish.png');
-        //this.load.image('background', './img/background.png');
-    }
+  preload() {
+    this.load.spritesheet("player", "./img/assets/walk.png", {
+      frameWidth: 200,
+      frameHeight: 200,
+    });
+    this.load.spritesheet("enemy", "./img/assets/fish.png", {
+      frameWidth: 100,
+      frameHeight: 100,
+    });
+    this.load.image("background", "./img/assets/background.jpg");
+  }
 
-    create() {
-        // Crea el fondo
-        //this.add.image(320, 180, 'background'); // Ajusta según el tamaño de tu fondo
+  create() {
+    // Crea el fondo
+    this.background = this.add.image(-200, 0, "background");
+    this.background.setScale(0.5);
+    this.background.setOrigin(0, 0);
 
-        // Crea el jugador
-        this.player = new Player(this, 100, 180);
+    // Crea el jugador
+    this.player = new Player(this, 100, 10);
+    this.player.setScale(0.4);
 
-        // Crea el enemigo
-        this.enemy = new Enemy(this, 400, 180);
+    // Crea el enemigo
+    this.enemy = new Enemy(this, 400, 180);
+    this.enemy.setScale(0.6);
 
-        // Configura los controles
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
+    // Configura los controles
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-    update() {
-        //this.player.update(this.cursors); // Actualiza el jugador
-        //this.enemy.update(); // Actualiza el enemigo
-    }
+    //methods block
+    // Crear la animación para el jugador
+    this.anims.create({
+      key: "player-walk",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 24 }),
+      frameRate: 25, // Velocidad de la animación
+      repeat: -1, // Repetir indefinidamente
+    });
+
+    // Crear la animación para el enemigo (pez)
+    this.anims.create({
+      key: "fish",
+      frames: this.anims.generateFrameNumbers("enemy", { start: 0, end: 24 }),
+      frameRate: 25, // Velocidad de la animación
+      repeat: -1, // Repetir indefinidamente
+    });
+  }
+
+  update() {
+    this.player.anims.play("player-walk", true);
+    //this.player.anims.play("fish", true);
+    this.player.update(this.cursors); // Actualiza el jugador
+    this.enemy.update(); // Actualiza el enemigo
+  }
 }
-
